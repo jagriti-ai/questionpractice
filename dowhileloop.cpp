55444addb46c0fde383e6598d78cc647fee54b2d
Jagriti <ruchigiri88@gmail.com>
2026-05-07 13:26:23 +0530

    added dowhile loop problem solving

---
+++ dowhileloop.cpp
@@ -0,0 +1,15 @@
+#include<iostream> //program to print number
+using namespace std;
+
+int main() {
+    int n=10;
+    int count=1;
+    do{ cout<<count<<" ";
+    count++;
+
+    }while(count<=n);
+
+    cout<<endl;
+
+    return 0;
+}
