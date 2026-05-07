2b63997cf69dd09777c1a6e4fe089bdebbb31c71
Jagriti <ruchigiri88@gmail.com>
2026-05-07 13:18:27 +0530

    added while loop problem practice

---
+++ whileloop.cpp
@@ -0,0 +1,16 @@
+#include<iostream> // print numbers 1 to 10
+
+using namespace std;
+
+int main() {
+
+    int n=10;
+    int count=1;
+    while(count<=n) {
+        cout<<count<<" ";
+        count++;
+}
+      cout<<endl;
+return 0;
+}
+
