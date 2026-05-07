62e661ad866a3abd71441787858d3e6999ed4211
Jagriti <ruchigiri88@gmail.com>
2026-05-07 13:24:37 +0530

    added for loop que practice

---
+++ forloop.cpp
@@ -0,0 +1,14 @@
+#include<iostream> //print numbers from 1 to 20
+using namespace std;
+
+int main() {
+
+    int n=20;
+    for(int i=1;i<=n;i=i+2){
+        cout<< i <<" ";
+    }
+    cout<<endl;
+    return 0;
+}
+
+
