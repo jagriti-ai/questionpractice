8718aba75389454ff1a6aeabdafd6dfa7c4bdafa
Jagriti <ruchigiri88@gmail.com>
2026-05-10 14:15:22 +0530

    Added calculating factorial of number "n"
    practice questions of my learning journey

---
+++ practiceque4.cpp
@@ -0,0 +1,21 @@
+// program to find factorial of a number
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+   long long factorial=1;
+// input from user
+   cout<<"enter n:";     
+   cin>>n;
+
+// calculate factorial using loop
+
+   for(int i=1;i<=n; i++){   
+factorial= factorial*i; // moving to next number in loop
+    cout<<i<< "! =" << factorial<<endl;  // o
+   }
+
+   return 0;
+}// end of program
