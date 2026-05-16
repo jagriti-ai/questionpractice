9c31ecdf62b3c8ed5a995cbc8af371c4b1f48f78
Jagriti <ruchigiri88@gmail.com>
2026-05-16 20:02:18 +0530

    Added a for loop program to print numbers backward

---
+++ loopquepractice4.cpp
@@ -0,0 +1,15 @@
+// program to print numbers in decreasing order
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n=5; //input
+
+    for(int i=n; i>0; i--){ //processing
+
+    cout<<i<<endl; // output
+    }
+    return 0;
+}
+//end of program
